0dc7b9cd4511b09cfaa6f7c985e9d64cfc6813fb
Chirag Patel <chirag@aurainteract.com>
2022-04-15 15:26:07 +0530

    final

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 app.use(cors());
-//app.use('/v1', router)
+app.use('/v1', router)
 app.use('/v1', (req,res)=>{
     res.send({data: 'Hello world'})
 })
