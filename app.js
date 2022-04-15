8c6de756a07c8da627b58b1be659f124bc8c1a2d
Chirag Patel <chirag@aurainteract.com>
2022-04-15 14:59:29 +0530

    testing1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyparser from 'body-parser';
-import router from './src/routes';
+//import router from './src/routes';
 import cors from 'cors';
 
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 app.use(cors());
-app.use('/v1', router)
+//app.use('/v1', router)
 app.use('/v1', (req,res)=>{
     res.send({data: 'Hello world'})
 })
